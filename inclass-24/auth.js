c8c879911980bf38b7cc3f744ef288e30b8d0322
prakharsdoshi <prakharsdoshi@gmail.com>
2016-11-15 23:13:15 -0600

    REDIS

---
+++ inclass-24/auth.js
@@ -0,0 +1,81 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const redis = require('redis').createClient('redis://h:p9s4kkvasg2i6jdgv1k8kud6dum@ec2-54-221-228-237.compute-1.amazonaws.com:9739')
+const md5 = require('md5')
+const cookieParser = require('cookie-parser')
+const app = express()
+app.use(bodyParser.json())
+app.use(cookieParser())
+
+const User = [];
+const cookieKey = 'sid';
+
+const registerUser =(req, res) => {
+     const salt =  Math.random().toString(36).substring(5);
+     User.push({"id" : User.length+1 ,"username" : req.body.username, "salt":salt, "hash" : md5(req.body.password+salt)})
+     res.send("Registered Successfully");
+}
+
+
+const loginUser =(req, res) => {
+
+	const response =  User.find(function(user) {
+		if(user.username === req.body.username)
+			{
+				return user.hash === md5(req.body.password + user.salt);	
+		}
+		else {
+			return false;
+		}
+		
+	});
+
+	if(typeof(response)==='object') { 
+		const sessionId = Math.random().toString(36).substring(2,7);
+		console.log()
+		redis.set(sessionId, req.body.username)
+		res.cookie(cookieKey,sessionId,{maxAge:3600*1000, httpOnly : true});
+
+		res.send({username : req.body.username, result : "success"}); 
+	}
+		else { res.sendStatus(401);}
+	}
+
+const logout= (req, res) => {
+  	if(req.cookies[cookieKey])  {
+  		redis.get(req.cookies[cookieKey], function(err, user ){
+  			if(!user){
+  				res.sendStatus(401)
+  			}
+  			else {
+  				console.log(req.cookies[cookieKey],"is mapped to ", user);
+				res.send("Bye"+user)
+			}  
+ 	    })
+	}
+	else{
+		res.send("No cookie Sent");
+  	}
+}
+
+app.post('/register/', registerUser);
+app.post('/login', loginUser);	 
+app.get('/logout', logout); 
+
+const port = process.env.PORT || 3000
+const server = app.listen(port, () => {
+     const addr = server.address()
+     console.log(`Server listening at http://${addr.address}:${addr.port}`)
+})	
+			
+
+
+
+
+
+
+
+
+
+
+
